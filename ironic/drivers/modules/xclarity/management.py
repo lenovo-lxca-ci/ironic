0359c86746a85889d1fd9641a0d033fe520aa169
jiapei2 <jiapei2@lenovo.com>
2018-12-11 09:49:34 +0000

    modify according to Shivanand's comments - tox -e docs

--- ironic/drivers/modules/xclarity/management.py
+++ ironic/drivers/modules/xclarity/management.py
@@ -98,7 +98,7 @@ class XClarityManagement(base.ManagementInterface):
         :param task: a task from TaskManager.
         :returns: a dictionary containing:
             :boot_device: the boot device, one of
-                [PXE, DISK, CDROM, BIOS, NONE]
+            [PXE, DISK, CDROM, BIOS, NONE]
             :persistent: Whether the boot device will persist or not
         :raises: InvalidParameterValue if the boot device is unknown
         :raises: XClarityError if the communication with XClarity fails
